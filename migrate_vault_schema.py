b6b601bde542b860e57074b5c71668a502a85c8e
skyflow-srivyshnavi <srivyshnavi.koduri@skyflow.com>
2025-07-02 10:23:03 +0530

    IE-456: updated connections config

--- migrate_vault_schema.py
+++ migrate_vault_schema.py
@@ -54,7 +54,7 @@ def main():
         print("-- Initializing Vault migration --")
         if VAULT_SCHEMA_CONFIG is not None and VAULT_SCHEMA_CONFIG == "config_file":
             print(f"-- Fetching the schema from the config file --")
-            with open("configs/connections/schema.json", "r") as file:
+            with open("configs/vaults/schema.json", "r") as file:
                 content = file.read()
                 schema = json.loads(content)
                 vault_details = {
